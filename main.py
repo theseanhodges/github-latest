32d0a9a684afe414cf19bc8cfaab2c517d10f140
Sean Hodges <sthodges@gmail.com>
2021-03-21 15:08:03 -0700

    Finish todo's

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    events = json.loads(
+        requests.get(f'https://api.github.com/users/{username}/events').content
+    )
 
-    print("COMPLETE THE TODOs")
+    print(events[0]['created_at'])
     
 
 
